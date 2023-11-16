ce1ff06c1e469f4d3bb990ffb245efbb2a967ac2
ThuyTrang45K <150211818+ThuyTrang45K@users.noreply.github.com>
2023-11-16 23:23:59 +0700

    Update Exercise66.py

--- Exercise66.py
+++ Exercise66.py
@@ -1,11 +1,10 @@
-# Vì không tìm thấy nội dung bài 51 nên nhóm em xin quy đổi điểm theo bảng điểm đại học DUE
 Dem=0
 S=0
 while True:
     DiemChu=input('Nhập điểm chữ (để trống để kết thúc): ')
     if not DiemChu:
         break
-    DiemChu_DiemSo={'A+': 4.0, 'A': 4.0, 'B+': 3.5, 'B': 3.0, 'C+': 2.5, 'C': 2.0, 'D+': 1.5, 'D': 1.0, 'F': 0.0}
+    DiemChu_DiemSo={'A+': 4.0, 'A': 4.0, 'A-': 3.7,'B+': 3.3, 'B': 3.0, 'B-': 2.7, 'C+': 2.3, 'C': 2.0, 'C-': 1.7, 'D+': 1.3, 'D': 1.0, 'F': 0.0}
     DiemSo=DiemChu_DiemSo.get(DiemChu, 0.0)
     Dem+=1
     S+=DiemSo
